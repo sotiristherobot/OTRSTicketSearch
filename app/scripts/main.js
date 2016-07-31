e91dd309b26d0feaf06549de8e3301ba1dae6d4f
Sotiris Ioannou <me@sotiris.engineer>
2016-07-31 17:00:25 -0400

    Added ticket object

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,14 @@
-console.log('\'Allo \'Allo!');
+(function () {
+
+  var ticket = function (id, title, content) {
+    this.id = id;
+    this.title = title;
+    this.content = content;
+  }
+
+
+  var ticket_test = new ticket(1, "kokos-tromeors", "o kokos einai apo tin kokia");
+
+  console.log(ticket_test);
+
+})();
